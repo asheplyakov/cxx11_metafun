ee29f25bc27ff295e8555006afb6cb30d24e0094
Alexey Sheplyakov <asheplyakov@altlinux.org>
2020-12-31 00:14:11 +0400

    N-ary function pointers type safe wrapper...
    ... without repetitive typing

---
+++ funcptr_variant/funcptr_variant.cpp
@@ -0,0 +1,131 @@
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include "mapbox/variant.hpp"
+#include "mapbox/variant_visitor.hpp"
+
+template<int ...> struct seq {};
+
+template<int N, int ...S> struct genseq : genseq<N-1, N-1, S...> { };
+
+template<int ...S> struct genseq<0, S...> {
+	typedef seq<S...> type;
+};
+
+template<int N, typename A>
+static inline auto get(const A& array)
+#if __cplusplus < 201402L
+->  decltype(array[N])
+#endif
+{
+	return array[N];
+}
+
+template<int N>
+struct array_unpacker {
+	using seq_type = typename genseq<N>::type;
+
+	template<typename F, typename C, int... S>
+	inline auto do_call(F& func, const C& params, seq<S...>)
+#if __cplusplus < 201402L
+	-> decltype(func(get<S>(params)...))
+#endif
+	{
+		return func(get<S>(params)...);
+	}
+
+	template<typename F, typename C>
+	inline auto call(F& func, const C& params)
+#if	__cplusplus < 201402L
+       	-> decltype(this->do_call(func, params, seq_type{})) 
+#endif
+	{
+		return do_call(func, params, seq_type{});
+	}
+
+};
+
+template<typename T> struct get_arity_s;
+
+template<typename R, typename ...Args>
+struct get_arity_s<R(*)(Args...)> {
+	static constexpr unsigned value = sizeof...(Args);
+};
+
+template<typename R, typename ...Args>
+struct get_arity_s<R(Args...)> {
+	static constexpr unsigned value = sizeof...(Args);
+};
+
+template<typename F>
+constexpr unsigned get_arity(const F& func) {
+	return get_arity_s<F>::value;
+}
+
+template<typename F, typename C>
+static inline auto call_with_array(const F& f, const C& packed_args)
+#if __cplusplus < 201402L
+	-> decltype(array_unpacker<get_arity_s<F>::value>{}.call(f, packed_args))
+#endif
+{
+	return array_unpacker<get_arity_s<F>::value>{}.call(f, packed_args);
+}
+
+template<typename ValT, typename ArgsT, int N> struct nary_func_declarator {
+	template<int x> struct dummy {
+		typedef ArgsT type;
+	};
+	template<typename X> struct helper;
+	template<int... S> struct helper<seq<S...>> {
+		typedef ValT (*type)(typename dummy<S>::type...);
+	};
+	typedef typename helper<typename genseq<N>::type>::type type;
+};
+
+
+// typedef double (*unary_double_funcp)(double);
+// typedef double (*binary_double_funcp)(double, double);
+// typedef double (*trinary_double_funcp)(double, double, double);
+// typedef double (*ternary_double_funcp)(double, double, double, double);
+// think of double (*double_funcp_8)(double, double, double, double, double, double, double, double);
+
+template<typename T> struct double_funcp_helper;
+template<int... S> struct double_funcp_helper<seq<S...>> {
+	typedef mapbox::util::variant<
+		typename nary_func_declarator<double, double, S>::type...>
+		type;
+};
+using double_funcp = double_funcp_helper<typename genseq<4>::type>::type;
+
+
+struct EvalVisitor {
+	double val_;
+	const std::vector<double>& args_;
+	EvalVisitor(const std::vector<double>& args) : val_(0), args_(args) { }
+	template<typename T>
+	inline void operator()(T fptr) {
+		this->val_ = call_with_array(fptr, this->args_);
+	}
+};
+
+static double minkowski(double t, double x, double y) {
+	std::printf("called with t=%g, x=%g, y=%g\n", t, x, y);
+	return t*t - x*x - y*y;
+}
+
+static inline double mysin(double x) {
+	std::printf("called with x=%g\n", x);
+	return std::sin(x);
+}
+
+int main(void) {
+	std::vector<double> args = { 5.0, 3.0, 4.0 };
+	double_funcp fp = &minkowski;
+	EvalVisitor visitor{args};
+	mapbox::util::apply_visitor(visitor, fp);
+	auto distance = visitor.val_;
+	double_funcp sinp = &mysin;
+	mapbox::util::apply_visitor(visitor, sinp);
+	return distance != 0.0;
+}
+// compile: g++ -std=c++11 -O2 -g -Wall array_unpack.cc
